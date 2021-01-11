769cedb760e60f5b8da535a649e07e81d2de2f6e
Sunil Shaw <34816778+erronny@users.noreply.github.com>
2021-01-11 23:35:35 +0530

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,24 @@
+
+<?php
+	return [
+		'TIME' => '12',
+		'SLOT' => '24',
+		'SLOT_TIME' => '30',
+		'SLOT_TOTAL' => '720',
+
+
+		'STUDIO_ID'=> 20,
+		'STUDIO_USER'=> 18,
+		'SUPER_ADMIN'=> 1,
+		'TRANSCRIPT'=> 19,
+		'TRANSLATOR'=> 25,
+		'CAPTION'=> 32,
+
+
+
+		"VENDOR"=>20,
+		"REPRASNTATIVE"=>18,
+		"AGENT"=>19,
+	];
+
+?>
